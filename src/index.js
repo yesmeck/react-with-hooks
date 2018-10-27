7230b23b109d26fc0054e2eeee049166198635c2
Wei Zhu <yesmeck@gmail.com>
2018-10-27 20:35:09 +0800

    Use native implementation if it's available

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
+import React from 'react';
 import withHooks from './withHooks';
+import * as hooks from './withHooks';
 
-export * from './withHooks';
+const useNative = !!React.useState;
 
-export default withHooks;
+export const useState = useNative ? React.useState : hooks.useState;
+export const useEffect = useNative ? React.useEffect : hooks.useEffect;
+export const useContext = useNative ? React.useContext : hooks.useContext;
+export const useReducer = useNative ? React.useReducer : hooks.useReducer;
+export const useCallback = useNative ? React.useCallback : hooks.useCallback;
+export const useMemo = useNative ? React.useMemo : hooks.useMemo;
+export const useRef = useNative ? React.useRef : hooks.useRef;
+export const useImperativeMethods = useNative ? React.useImperativeMethods : hooks.useImperativeMethods;
+export const useMutationEffect = useNative ? React.useMutationEffect : hooks.useMutationEffect;
+export const useLayoutEffect = useNative ? React.useLayoutEffect : hooks.useLayoutEffect;
+export default useNative ? (fn) => fn : withHooks;
